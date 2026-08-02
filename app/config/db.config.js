b5dd638ff8e7cb9f9f846a2eb8d6cd0f81c50cdf
favasquezUMG <fvasquezl6@miumg.edu.gt>
2026-08-02 01:23:28 -0600

    deploy on Render was improved

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+if(!process.env.DATABASE_URL){
+    console.error("ERROR: La variable DATABASE_URL no esta definida en las variables de entorno.")
+}
+
 module.exports = {
     URL: process.env.DATABASE_URL,
     dialect: "postgres",
